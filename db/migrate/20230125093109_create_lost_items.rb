a1bddddf49ec14a651197514d50ec1967c052e2c
geogrey12muiruri <greggambugua@gmail.com>
2023-01-25 12:37:00 +0300

    first commit

---
+++ db/migrate/20230125093109_create_lost_items.rb
@@ -0,0 +1,11 @@
+class CreateLostItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lost_items do |t|
+      t.string :name
+      t.text :description
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
